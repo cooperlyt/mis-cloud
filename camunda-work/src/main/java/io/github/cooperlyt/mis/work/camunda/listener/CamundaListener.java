f5e049c923804fb25da4fa88b7ee86a75f36a387
cooper <lyt3131@163.com>
2023-10-09 21:49:26 +0800

    DELETE, ABORT process

--- camunda-work/src/main/java/io/github/cooperlyt/mis/work/camunda/listener/CamundaListener.java
+++ camunda-work/src/main/java/io/github/cooperlyt/mis/work/camunda/listener/CamundaListener.java
@@ -2,15 +2,17 @@ package io.github.cooperlyt.mis.work.camunda.listener;
 
 import io.github.cooperlyt.mis.work.camunda.mq.ProcessChangeService;
 import io.github.cooperlyt.mis.work.message.WorkChangeMessage;
+import io.github.cooperlyt.mis.work.message.WorkStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.IdentityService;
 import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.identity.User;
 import org.camunda.bpm.engine.impl.history.event.HistoryEvent;
-import org.camunda.bpm.engine.repository.ProcessDefinition;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.model.bpmn.instance.BaseElement;
 import org.camunda.bpm.model.bpmn.instance.camunda.CamundaProperties;
@@ -18,6 +20,7 @@ import org.camunda.bpm.model.bpmn.instance.camunda.CamundaProperty;
 import org.camunda.bpm.spring.boot.starter.event.ExecutionEvent;
 import org.camunda.bpm.spring.boot.starter.event.TaskEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -30,6 +33,8 @@ public class CamundaListener {
 
   private final TaskService taskService;
 
+  private final RuntimeService runtimeService;
+
   private final ProcessChangeService processChangeService;
 
   private final RepositoryService repositoryService;
@@ -37,9 +42,10 @@ public class CamundaListener {
   private final IdentityService identityService;
 
   public CamundaListener(TaskService taskService,
-                         ProcessChangeService processChangeService,
+                         RuntimeService runtimeService, ProcessChangeService processChangeService,
                          RepositoryService repositoryService, IdentityService identityService) {
     this.taskService = taskService;
+    this.runtimeService = runtimeService;
     this.processChangeService = processChangeService;
     this.repositoryService = repositoryService;
     this.identityService = identityService;
@@ -57,9 +63,10 @@ public class CamundaListener {
     // handle mutable task event
   }
 
+  @Order(1)
   @EventListener(condition="#taskEvent.eventName=='complete'")
-  public void onTaskEvent(TaskEvent taskEvent) {
-    log.debug("immutable task event: {} by TaskEvent", taskEvent.getEventName());
+  public void onTaskCompleteEvent(TaskEvent taskEvent) {
+    log.debug("immutable task complete event: {} by TaskEvent", taskEvent.getEventName());
 
 
     Task task = taskService.createTaskQuery().taskId(taskEvent.getId()).initializeFormKeys().singleResult();
@@ -85,7 +92,6 @@ public class CamundaListener {
       throw new RuntimeException(e);
     }
 
-
 //    getCamundaProperties(taskEvent.getProcessDefinitionId(),taskEvent.getTaskDefinitionKey())
 //        .stream().filter(p -> p.getCamundaName().equals("type")).findFirst()
 //        .ifPresent(type -> {
@@ -93,8 +99,25 @@ public class CamundaListener {
 //        });
   }
 
-  @EventListener
+  @Order(2)
+  @EventListener(condition="#taskEvent.eventName=='delete'")
+  public void onTaskDeleteEvent(TaskEvent taskEvent){
+    log.debug("immutable task delete event: {} by TaskEvent", taskEvent.getEventName());
+
+    ProcessInstance processInstance = runtimeService.createProcessInstanceQuery()
+        .processInstanceId(taskEvent.getProcessInstanceId())
+        .singleResult();
+    try {
+      processChangeService.statusChange(Long.parseLong(taskEvent.getCaseInstanceId()),
+          processInstance.isEnded() ? WorkStatus.DELETED : WorkStatus.ABORT,
+          taskEvent.getProcessDefinitionId());
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
   public void onExecutionEvent(DelegateExecution executionDelegate) {
+
     log.debug("immutable executionDelegate event: {}", executionDelegate.getEventName());
     // handle mutable execution event
   }
